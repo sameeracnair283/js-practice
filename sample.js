cb7bb70a81ede0207dec16a8a39265ac6bfcd16c
unknown <sameeracnair283@gmail.com>
2026-04-10 15:02:46 +0530

    secnd commmit

--- sample.js
+++ sample.js
@@ -1,8 +1,71 @@
-let a=10;
-console.log(a);
-let name="Maaa"
-console.log(name);
-let place="javascript";
-console.log(place);
-var n=5;
-console.log(n);
+// let a=10;
+// console.log(a);
+// let name="Maaa"
+// console.log(name);
+// let place="javascript";
+// console.log(place);
+// var n=5;
+// console.log(n);
+// const sum =5+3;
+// const diff=10-2;
+// const p=4*2;
+// const q=8/2;
+// console.log(sum,diff,p,q);
+// let s=10;
+// s+=5;
+// s*=2;
+// console.log(s);
+// console.log(10>5);
+// console.log(10==="10");
+// const c = true, b =false;
+// console.log(a&&b);
+// console.log(a||b);
+// const num = 5;
+// if(num>0){
+//     console.log("The number is positive");
+// };
+// let number = -10;
+// if (num >0)
+//     console.log("the number is positive");
+// else
+//     console.log("the number is negative");
+
+// let numb=5;
+// switch(numb){
+//     case 0:
+//         console.log("Number is zero");
+//         break;
+//     case 1:
+//         console.log("number is one");
+//         break;
+//     case 2:
+//         console.log("number is two");
+//         break;
+//     default:
+//         console.log("number is greater than 2");
+// };
+
+let num = 10;
+
+switch (num) {
+    case 0:
+        console.log("Number is zero.");
+        break;
+    case 1:
+        console.log("Nuber is one.");
+        break;
+    case 2:
+        console.log("Number is two.");
+        break;
+    case 3:
+        console.log("Number is three.");
+        break;
+    case 4:
+        console.log("Number is four.");
+        break;
+    case 5:
+        console.log("Number is five.");
+        break;
+    default:
+        console.log("Number is greater than 5");
+};
