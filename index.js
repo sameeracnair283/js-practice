c09c5f6877f3b2fb224240aa9dac9096e7c7ab4f
unknown <sameeracnair283@gmail.com>
2026-04-11 08:32:24 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+console.log("hello world");
+let name="sameera";
+console.log(name);
+let a=10;
+let b=3;
+console.log(a+b);
+let num=7;
+if (num % 2==0){
+    console.log("even number");
+}else{
+    console.log("odd number");
+}
+let age=15;
+if(age>=18){
+    console.log("adult");
+}else{
+    console.log("minor");
+}
+for(let i=1;i<10;i++){
+    console.log(i);
+}
+for (let i=1;i<=20;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+let sum=0;
+for(let i=1;i<=10;i++){
+    sum+=1;
+}
+console.log(sum);
+function hey (){
+    console.log("heyy");
+}
+hey();
+function plus(a,b){
+    return a+b;
+}
+let result=plus(5,3);
+console.log(result);
+function evenodd(num){
+    if(num%2==0){
+        console.log("even number");
+    }else{
+        console.log("odd number");
+
+    }
+}
+evenodd(7);
+let x=5;
+let y=6;
+if(x>y){
+    console.log(x);
+}else{
+    console.log(y);
+}
+let number=10;
+if(number>0){
+    console.log("positive");
+}else if(number<0){
+    console.log("negative");
+}else{
+    console.log("zero");
+}
